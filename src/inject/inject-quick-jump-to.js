c3bcefb0e22efa0a7bcd4634cda3dd6fb0e74593
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-05 20:14:24 +0200

    well, we made it look a bit better

--- src/inject/inject-quick-jump-to.js
+++ src/inject/inject-quick-jump-to.js
@@ -19,16 +19,4 @@ export function injectQuickJumpTo (document) {
     event.preventDefault();
     window.location = `/course/view.php?id=${searchInput.value}`
   })
-
-  const nav = document.querySelector('.navbar ul.nav')
-
-  const listItem = document.createElement('li')
-  const link = document.createElement('a')
-
-  link.setAttribute('href', '/my')
-  link.innerText = 'Mein Moodle'
-  link.title = 'Mein Moodle'
-
-  listItem.appendChild(link)
-  nav.insertBefore(listItem, nav.firstElementChild)
 }
